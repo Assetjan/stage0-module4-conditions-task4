02287a3e1ed51ef197733ecbc939f6192feefa33
Assetjan <asetziyabek@gmail.com>
2023-01-19 20:48:42 +0600

    done

--- src/main/java/school/mjc/stage0/conditions/task4/TernaryAssignment.java
+++ src/main/java/school/mjc/stage0/conditions/task4/TernaryAssignment.java
@@ -3,7 +3,7 @@ package school.mjc.stage0.conditions.task4;
 public class TernaryAssignment {
     public void assignAndPrintBasedOnWhichBigger(int first, int second) {
         int result;
-        result = first > second ? first : second;
+        result = first < second ? second : first;
         System.out.println(result);
 
 
--- src/main/java/school/mjc/stage0/conditions/task4/TernaryPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/TernaryPrinter.java
@@ -2,8 +2,8 @@ package school.mjc.stage0.conditions.task4;
 
 public class TernaryPrinter {
     public void printWhichIsBigger(int first, int second) {
-        int res;
-        res = first > second ? first : second;
+        String res;
+        res = first > second ? "first" : "second";
         System.out.println(res);
     }
 }
